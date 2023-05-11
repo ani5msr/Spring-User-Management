964b3dc464c9e560e998d89a8b375015c5bf5328
Anirudh Mishra <anirudh5msr@gmail.com>
2023-05-12 00:03:38 +0530

    Implemented UserDetails class and added roles enum

---
+++ com/src/main/java/base/usermanage/com/model/Role.java
@@ -0,0 +1,5 @@
+package base.usermanage.com.model;
+
+enum Role {
+		ADMIN, USER
+}
--- com/src/main/java/base/usermanage/com/model/UserModel.java
+++ com/src/main/java/base/usermanage/com/model/UserModel.java
@@ -17,5 +17,58 @@ import java.util.Collections;
 @AllArgsConstructor
 @Entity(name = "Users")
 public class UserModel implements UserDetails {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+	
+	private String email;
+
+	private String password;
+	@Builder.Default
+	private Boolean locked = false;
+
+	@Builder.Default
+	private Boolean enabled = false;
+	@Builder.Default
+	private Role userRole = Role.USER;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+
+		final SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(userRole.name());
+		return Collections.singletonList(simpleGrantedAuthority);
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return !locked;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return enabled;
+	}
 	
 }
