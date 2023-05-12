7997b67fe0ca939ae98577957b3ac6ead84dce1e
Anirudh Mishra <anirudh5msr@gmail.com>
2023-05-12 20:28:42 +0530

    Chaged to Role class

--- com/src/main/java/base/usermanage/com/model/Role.java
+++ com/src/main/java/base/usermanage/com/model/Role.java
@@ -1,5 +1,16 @@
 package base.usermanage.com.model;
 
-enum Role {
-		ADMIN, USER
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Role {
+    private Long roleId;
+    private String roleName;
 }
