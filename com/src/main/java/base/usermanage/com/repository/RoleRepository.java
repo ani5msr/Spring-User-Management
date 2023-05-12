7401c4e5f0cc91eba546f10523c635220200d590
Anirudh Mishra <anirudh5msr@gmail.com>
2023-05-12 23:14:24 +0530

    Changes

--- com/src/main/java/base/usermanage/com/repository/RoleRepository.java
+++ com/src/main/java/base/usermanage/com/repository/RoleRepository.java
@@ -2,6 +2,8 @@ package base.usermanage.com.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoleRepository extends JpaRepository<RoleEntity, Long>{
+import base.usermanage.com.entity.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
 
 }
